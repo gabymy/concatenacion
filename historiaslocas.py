66e0db84ca4286991fe7789f5fd6aa80cdaf7e7d
Gabriela Mutilva <66925220+gabymy@users.noreply.github.com>
2021-09-25 15:26:47 -0300

    Add files via upload

---
+++ historiaslocas.py
@@ -0,0 +1,9 @@
+# concatenacion de cadenas de caracteres
+adj = input("Adjetivo:  ")
+verbo1 = input("Verbo:  ")
+sustantivo = input("Sustantivo:  ")
+
+
+mablib = f"Cada mañana es tan {adj}...Siempre estoy de mal humor cuando me {verbo1}. Solo necesito tomar mi cafe en {sustantivo}"
+
+print(mablib)
